789b0c37461817437e3de6f418bbd875422cb3f5
sieun park <89372116+singny@users.noreply.github.com>
2024-08-20 04:30:24 +0000

    Update HttpServer.java

--- HttpServer.java
+++ HttpServer.java
@@ -18,14 +18,13 @@ public class HttpServer {
     private static final Logger logger = Logger.getLogger(HttpServer.class.getCanonicalName());
     private static final int NUM_THREADS = 50;
     private static final String INDEX_FILE = "index.html";
-    private final Map<String, File> virtualHosts;
+    private final Map<String, ServerConfig.HostConfig> virtualHosts;
     private final int port;
 
-    public HttpServer(Map<String, File> virtualHosts, int port) throws IOException {
-        this.virtualHosts = virtualHosts;
-        this.port = port;
+    public HttpServer(ServerConfig config) {
+        this.virtualHosts = config.getHosts(); // ServerConfig에서 가져온 hosts
+        this.port = config.getPort();
     }
-
     public void start() throws IOException {
         ExecutorService pool = Executors.newFixedThreadPool(NUM_THREADS);
         try (ServerSocket server = new ServerSocket(port)) {
@@ -46,13 +45,13 @@ public class HttpServer {
         Map<String, File> virtualHosts = new HashMap<>();
         
         // 호스트별 루트 디렉터리를 설정합니다.
-        virtualHosts.put("a.com", new File("src/main/resources/a_com_root"));
-        virtualHosts.put("b.com", new File("src/main/resources/b_com_root"));
-        virtualHosts.put("default", new File("src/main/resources/a_com_root"));
+//        virtualHosts.put("a.com", new File("src/main/resources/a_com_root"));
+//        virtualHosts.put("b.com", new File("src/main/resources/b_com_root"));
+//        virtualHosts.put("default", new File("src/main/resources/a_com_root"));
         
         // set the port to listen on
 //        int port;
-        int port = 9090;
+//        int port = 9090;
 //        try {
 //            port = Integer.parseInt(args[0]);
 //            if (port < 0 || port > 65535) port = 80;
@@ -61,7 +60,9 @@ public class HttpServer {
 //        }
      
         try {
-            HttpServer webserver = new HttpServer(virtualHosts, port);
+            ServerConfig config = new ServerConfig("src/main/resources/config.json");
+            HttpServer webserver = new HttpServer(config);
+//            HttpServer webserver = new HttpServer(virtualHosts, port);
             webserver.start();
         } catch (IOException ex) {
             logger.log(Level.SEVERE, "Server could not start", ex);
